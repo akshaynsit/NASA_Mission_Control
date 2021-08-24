c5679ba0075efbf7d4217eeb52a9f4425c719c1d
akshaynsit <akshaymilan@users.noreply.github.com>
2021-08-25 01:33:45 +0530

    common package
    Created a common package for both Server and Client

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,50 @@
-const planets = [];
+const fs =require('fs');
+const path = require('path');
+const parse = require('csv-parse');
+  
+const { Stream } = require('stream');
 
-module.exports = planets;
+const habitablePlanets =[]
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] ==='CONFIRMED' &&
+       planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6 ;
+}
+
+/* while running for the first time,our data may or may not be uploaded fully coz we are getting the data in Stream,so in that case we may 
+get empty Stream...solution --> for that we will use JS promise */
+function loadPlanetData(){
+    return new Promise((resolve,reject)=> {
+        fs.createReadStream(
+            path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+          )
+        .pipe(parse({
+            comment:'#',
+            columns:true,
+        }))
+        .on('data',(data)=>{
+            if(isHabitablePlanet(data)){
+                habitablePlanets.push(data);
+            } 
+        })
+        .on('error',(err)=>{
+            console.log(err);
+            reject(err);
+        })
+        .on('end',() => {
+            /* console.log(habitablePlanets)
+            console.log('done') */
+            /* console.log(habitablePlanets.map((planet)=>{
+                return planet['kepler_name'];
+
+            } )); */
+            console.log(` Total ${habitablePlanets.length} habitable planet found`);
+            resolve();
+            });
+    });}
+
+
+module.exports = {
+    loadPlanetData,
+    planets:habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const { start } = require('repl');
 
 const app = require('./app')
+const { loadPlanetData} = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;//React Port run on 3000,port to run as a enviromental variable or 8000
 
 const server = http.createServer(app);
-server.listen(PORT,()=>{
-    console.log(`Running on Port ${PORT}...`)
-});
+async function startServer(){
+    await loadPlanetData();
+    server.listen(PORT,()=>{
+        console.log(`Running on Port ${PORT}...`)
+    });
+}
+
+startServer();
+
