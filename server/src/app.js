69eb3820c35f5e8e7d67ff06d1f3e6cf3a980c07
akshaynsit <akshaymilan@users.noreply.github.com>
2021-08-27 01:25:28 +0530

    One Port
    One port for Both Client and Server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router')
 app.use(cors({
@@ -17,5 +18,9 @@ var corsOptions = {
   }
 } */
 app.use(express.json());//Parse any incoming JSON from body of incoming request
+app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetsRouter);
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '..','public','index.html'))
+})
 module.exports = app;
