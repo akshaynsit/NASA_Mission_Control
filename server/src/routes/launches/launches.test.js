ef85c93f4e777ca8b13d79455d25e4d83ba258a4
akshaynsit <akshaymandhare@gmail.com>
2021-09-07 13:13:49 +0530

    Adding the Test using JEST

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,17 @@
+//Context of writing API TEST
+//TEST fixure using Describe Function
+describe('Test GET /launches',()=>{
+    test('It should respond with 200 success',()=>{
+        const response=200;
+        //assertion
+        expect(response).toBe(200);
+    })
+})
+
+describe('Test POST /launches',()=>{
+    test('It should respond with 200 success',()=>{
+
+    });
+    test('It should catch mission required Properties',()=>{});
+    test('It should catch catch invalid dates',()=>{})
+});
